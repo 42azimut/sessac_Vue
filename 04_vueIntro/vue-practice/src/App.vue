2899b6e56a12a3296897c5ca9d6fedd807154c25
42azimut <azimutuniverse@gmail.com>
2021-12-06 12:23:34 +0900

    31. 리스트 렌더링

--- 04_vueIntro/vue-practice/src/App.vue
+++ 04_vueIntro/vue-practice/src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <div>
-    <!-- <h2 v-if="showName">My name is {{ user.name }}</h2>
-    <h2 v-else>이름을 보여줄수 없음 v-else 위에 붙여서! 사용!</h2>
-    <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
-    <h2 v-else-if="user.age > 14 && user.age < 20">청소년 이군요!</h2>
-    <h2 v-else>당신은 어른 입니다.</h2> -->
-    <h2 v-if="!showName">{{ user.name }} IF</h2>
-    <h2 v-show="!showName">{{ user.name }} Show</h2>
-    <!-- 위의 실제 태그 <h2 style="display: none;">Azimut Show</h2> 
-    v-if 데이터를 아에 태그 생성 안함(새로운 데이터 필요하면 사용) 
-    if-show는 랜더링 하여 태그는 나오고 안보이게 함-->
+    <h1>Hello Vue JS!</h1>
+    <!-- 
+    <div>
+      {{ fruites }}
+    </div>
+    
+    <div>
+     <ul>
+        <li v-for="(fruit, index) in fruites" :key="index">
+          {{ index + 1 }} - {{ fruit }}
+        </li>
 
-    <!-- 의미가 없는템플릿 태그 이용하기 -->
-    <ul>
-      <template v-if="question === 'frontend'">
-        <li>Html 재밋나요?</li>
-        <li>CSS 재밌나요?</li>
-        <li>자바스크립트 재밌나요?</li>
-      </template>
+        <li v-for="(fruit, index) of fruites" :key="index">
+          {{ index + 1 }} - {{ fruit }}
+        </li>
 
-      <template v-else>
-        <li>JAVA 재밌나요?</li>
-        <li>python 재밌나요?</li>
-        <li>C++ 재밌나요?</li>
-      </template>
-    </ul>
+        <h2 v-for="(value, key, index) in user" :key="key">
+          {{ index + 1 }}. {{ key }} == >> {{ value }}
+        </h2>
+
+        <p v-for="n in 10" :key="n">{{ n }}</p>
+      </ul> 
+    </div> -->
+
+    <div v-for="(animal, animalIndex) in animals" :key="animalIndex">
+      <h2>Animal ===> {{ animal.name }}</h2>
+      <h3>food</h3>
+      <li v-for="(food, foodIndex) in animal.favorites" :key="foodIndex">
+        {{ food }}
+      </li>
+    </div>
   </div>
 </template>
 
@@ -33,13 +39,21 @@ export default {
   name: "App",
   data() {
     return {
-      question: "back",
-      showName: true,
-      user: {
-        name: "Azimut",
-        age: 20,
-        job: "developer",
-      },
+      // fruites: ["apple", "banana", "tomato", "melon", "banana", "tomato"],
+      // user: {
+      //   name: "Jayden",
+      //   age: 200,
+      //   job: "realtor",
+      // },
+      animals: [
+        { name: "monkey", size: "medium", favorites: ["banana", "chiken"] },
+        { name: "lion", size: "big", favorites: ["meat", "beef"] },
+        {
+          name: "rat",
+          size: "small",
+          favorites: ["cheese", "rice", "돼지고기"],
+        },
+      ],
     };
   },
 };
