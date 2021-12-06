0e3928aa4537185cbe6a1c22048923b11a6171b5
42azimut <azimutuniverse@gmail.com>
2021-12-06 13:14:52 +0900

    33. 메서드 사용

--- 04_vueIntro/vue-practice/src/App.vue
+++ 04_vueIntro/vue-practice/src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div>
-    <h1>Hello Vue JS! 31. 조건부 리스트 랜더링</h1>
-    <ul>
-      <template v-for="(city, index) in cities" :key="index">
-        <li v-if="city.province === '경상도'">
-          {{ city.name }}
-        </li>
-      </template>
-    </ul>
+    <h1>Hello Vue JS! 32. 메서드</h1>
+    <h2>5년뒤 :: {{ add(5) }}</h2>
+    <h2>동갑의 10명이 있다면 나이 총 합은? {{ multiply(age, 10) }}</h2>
+    <h2>{{ getTotalScore(100) }}</h2>
   </div>
 </template>
 
@@ -16,27 +12,21 @@ export default {
   name: "App",
   data() {
     return {
-      cities: [
-        { name: "서울", province: "경기도" },
-        { name: "대전", province: "충청도" },
-        { name: "부산", province: "경상도" },
-        { name: "경주", province: "경상도" },
-        { name: "송도", province: "경기도" },
-        { name: "광주", province: "전라도" },
-      ],
+      age: 30,
     };
+  },
+  methods: {
+    add(num) {
+      return this.age + num;
+    },
+    multiply(num1, num2) {
+      return num1 * num2;
+    },
+    getTotalScore(num) {
+      return this.multiply(num, num);
+    },
   },
 };
 </script>
 
-<style>
-#title {
-  color: red;
-  background: yellow;
-}
-
-#content {
-  color: blue;
-  background: pink;
-}
-</style>
+<style></style>
