5d4557350b70053ae0b40f9fe59c09fe01dbe4cb
42azimut <azimutuniverse@gmail.com>
2021-12-08 12:14:29 +0900

    41 component 좀 어려웠다! list -> item 쪼개서 연결하기! 그리고 v-bind=''

--- 04_vueIntro/vue-practice/src/App.vue
+++ 04_vueIntro/vue-practice/src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <GreetingUser
-      :username="username"
-      :numberOfVisit="numberOfVisit"
-      :siteInfo="siteInfo"
-    />
-    <button @click="changeName">change</button>
+    <GreetingUser id="greeting" />
   </div>
 </template>
 
@@ -14,21 +9,22 @@ import GreetingUser from "./components/GreetingUser";
 export default {
   name: "App",
   components: {
+    //ProductList,
     GreetingUser,
   },
   data() {
     return {
-      username: "Azimut",
-      numberOfVisit: undefined,
-      siteInfo: undefined,
+      // products: [
+      //   { id: 0, name: "TV", price: 500000, company: "LG" },
+      //   { id: 1, name: "전자렌지", price: 200000, company: "삼성" },
+      //   { id: 2, name: "맥북프로", price: 1500000, company: "애플" },
+      //   { id: 3, name: "책상", price: 150000, company: "이케아" },
+      //   { id: 4, name: "모니터", price: 340000, company: "삼성" },
+      // ],
     };
   },
   directives: {},
-  methods: {
-    changeName() {
-      this.username = "Tesla";
-    },
-  },
+  methods: {},
   computed: {},
   watch: {},
 };
--- 04_vueIntro/vue-practice/src/components/GreetingUser.vue
+++ 04_vueIntro/vue-practice/src/components/GreetingUser.vue
@@ -1,11 +1,14 @@
 <template>
-  <h2>Hello {{ username }}!</h2>
-  <h3>오늘이 {{ numberOfVisit }} 번째 방문입니다.</h3>
-  <h3>사이트 이름은 {{ siteInfo.name }} 입니다.</h3>
+  <div>
+    <h2 v-bind="$attrs">Hello {{ username }}!</h2>
+    <h3>오늘이 {{ numberOfVisit }} 번째 방문입니다.</h3>
+    <h3>사이트 이름은 {{ siteInfo.name }} 입니다.</h3>
+  </div>
 </template>
 <script>
 export default {
   name: "Greeting User",
+  inheritAttrs: false,
   props: {
     username: String,
     numberOfVisit: {
