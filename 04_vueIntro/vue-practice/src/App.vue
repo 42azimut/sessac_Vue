57706a2423607093f5968c047956ad2311fe6fec
42azimut <azimutuniverse@gmail.com>
2021-12-09 11:00:01 +0900

    44 HTTP Request : axios, then

--- 04_vueIntro/vue-practice/src/App.vue
+++ 04_vueIntro/vue-practice/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
-    <h2>Hello TelePort</h2>
-    <teleport to="#extra-modal" :disabled="isTeleport">
-      <div class="modal">
-        this is modal window!
-        <button @click="isTeleport = !isTeleport">teleport toggle</button>
-      </div>
-    </teleport>
-    <teleport to="#extra-modal">
-      <div class="modal2">this is modal 2</div>
-    </teleport>
+    <h2>Hello HTTP Request</h2>
+    <TodoList />
   </div>
 </template>
 
 <script>
+import TodoList from "./components/http/TodoList.vue";
 export default {
   name: "App",
-  components: {},
+  components: { TodoList },
   data() {
-    return {
-      isTeleport: true,
-    };
+    return {};
   },
   directives: {},
   methods: {},
@@ -29,18 +20,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.modal {
-  position: fixed;
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  font-size: 36px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+<style scoped></style>
