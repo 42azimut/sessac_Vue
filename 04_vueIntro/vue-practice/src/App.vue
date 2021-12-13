4fbb5ee14813755b024747edb5a417080d886610
42azimut <azimutuniverse@gmail.com>
2021-12-13 19:48:59 +0900

    52 composition computed and watch

--- 04_vueIntro/vue-practice/src/App.vue
+++ 04_vueIntro/vue-practice/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Hello 48.Composition API</h2>
+    <h2>Hello 52.Composition computed & watch</h2>
   </div>
   <TestComponent />
 </template>
--- 04_vueIntro/vue-practice/src/components/composition/TestComponent.vue
+++ 04_vueIntro/vue-practice/src/components/composition/TestComponent.vue
@@ -1,41 +1,87 @@
 <template>
   <h2>{{ username }}</h2>
-  <button @click="changeName">Chagnge User Name</button>
-  <h2>제품명 : {{ name }}, 가격: {{ price }}</h2>
-  <button @click="changeProduct">제품 바꾸기</button>
+  <hr />
   <div>
-    <input type="text" v-model="username" />
+    price <input type="text" v-model="price" /> amout
+    <input type="text" v-model="amount" />
+    <h3>Total Price : {{ totalPrice }}</h3>
+  </div>
+  <hr />
+  <div>
+    first <input type="text" v-model="home.city" /> last
+    <input type="text" v-model="last" />
+    <h3>Full name :: {{ fullName }}</h3>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from "vue";
+import { ref, computed, reactive, toRefs, watch } from "vue";
 export default {
   name: "TestComponent",
   setup() {
     const username = ref("azimut-ref");
+    const price = ref(4000);
+    const amount = ref(1);
 
-    function changeName() {
-      username.value = "Messsi";
-    }
-
-    const state = reactive({
-      name: "TV",
-      price: 100,
+    const totalPrice = computed(() => {
+      return price.value * amount.value;
+    });
+    watch(price, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
     });
 
-    function changeProduct() {
-      state.name = "세탁기";
-      state.price = 4000;
-    }
+    const state = reactive({
+      first: "Code",
+      last: "Avengers",
+      home: {
+        city: "Seoul",
+        type: "apt",
+      },
+    });
+
+    const fullName = computed(function () {
+      return `${state.first} ${state.last}`;
+    });
+    watch(
+      () => {
+        return state.first;
+      },
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      }
+    );
+    watch(
+      () => {
+        return { ...state.home };
+      },
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      },
+      { deep: true }
+    );
 
     return {
       username,
-      changeName,
-      changeProduct,
+      price,
+      amount,
+      totalPrice,
+      fullName,
       ...toRefs(state),
     };
   },
+
+  // 기존 컴퓨티드 속성 이용한 방법 위(셋업 api)
+  // data() {
+  //   return {
+  //     price: 4000,
+  //     amount: 1,
+  //   };
+  // },
+  // computed: {
+  //   totalPrice() {
+  //     return this.price * this.amount;
+  //   },
+  // },
 };
 </script>
 
