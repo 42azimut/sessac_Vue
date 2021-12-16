95388bda84b9e6e3e41e6a07cfff8cf058d9d70b
42azimut <azimutuniverse@gmail.com>
2021-12-16 15:52:03 +0900

    날씨앱 마무리! .env

--- 04_vueIntro/vue-weather-app/src/mixins/weatherMixin.js
+++ 04_vueIntro/vue-weather-app/src/mixins/weatherMixin.js
@@ -6,7 +6,8 @@ const weatherMixin = {
   },
   methods: {
     async getWeatherInfo(city) {
-      const API_KEY = "5f61e1133d8c9ed8c6e9bf6bb5dd4b64";
+      console.log(process.env.VUE_APP_API_KEY);
+      const API_KEY = process.env.VUE_APP_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.code}&appid=${API_KEY}`;
       const res = await axios.get(url);
       const { main, wind, weather } = res.data;
