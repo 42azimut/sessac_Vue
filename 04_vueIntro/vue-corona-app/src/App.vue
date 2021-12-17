e9569233d698baf1dc33032993bdb2d36adacfd7
42azimut <azimutuniverse@gmail.com>
2021-12-17 10:46:49 +0900

    56-63 Corona API 컴포넌트 템플릿 _ Element Plus 사용

---
+++ 04_vueIntro/vue-corona-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <BasicLayout />
+</template>
+
+<script>
+import BasicLayout from "./components/layouts/BasicLayout.vue";
+
+export default {
+  name: "App",
+  components: {
+    BasicLayout,
+  },
+};
+</script>
+
+<style></style>
---
+++ 04_vueIntro/vue-corona-app/src/components/layouts/BasicLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container class="main">
+    <el-header><BasicHeader /></el-header>
+    <el-container>
+      <el-aside width="200px"><SideMenu /></el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+import BasicHeader from "./BasicHeader.vue";
+import SideMenu from "./SideMenu";
+export default {
+  name: "basicLayout",
+  components: {
+    BasicHeader,
+    SideMenu,
+  },
+};
+</script>
+
+<style>
+.el-header {
+  background: #409eff;
+  color: #fff;
+  display: flex;
+  align-items: center;
+}
+
+.main {
+  height: 100%;
+}
+</style>
